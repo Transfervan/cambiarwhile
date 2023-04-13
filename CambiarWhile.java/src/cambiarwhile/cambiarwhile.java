8987b423fd2de64ee32ff012e244b65aea1ebf6e
Transfervan <129233255+Transfervan@users.noreply.github.com>
2023-04-13 08:34:19 -0400

    version 1 cambiarwhile

---
+++ CambiarWhile.java/src/cambiarwhile/cambiarwhile.java
@@ -0,0 +1,26 @@
+package cambiarwhile;
+
+public class cambiarwhile {
+
+		public static void main(String[] args) {
+			int i = 0;
+			// miestras i sea menor que 50 hara ciclo, llega hasta 50 y se sale
+			while (i<50) {
+			i+=1;
+			System.out.printf("Iteración %d\n", i);
+	}
+		
+		i = 1;
+		// muestra mientras i sea menor o igual a 50
+		do {
+			System.out.printf("Iteración %d\n", i);
+			i+=1;
+		 }while  (i<=50);
+		 }
+
+
+}
+
+
+
+
---
+++ CambiarWhile.java/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author trans
+ *
+ */
+module CambiarWhile.java {
+}
